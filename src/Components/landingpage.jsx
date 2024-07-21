8711ffa7b16f443d88197df6b28edd1cfce37d05
Nosihle Mthembu <bongekam16@gmail.com>
2024-07-22 00:40:57 +0200

    home

--- src/Components/landingpage.jsx
+++ src/Components/landingpage.jsx
@@ -1,6 +1,29 @@
 
 export default function Landing(){
     return (
-        <h1>hi</h1>
+        <>
+            <ul>
+                <div className="search">
+                <li action="action_page.php">
+                <input type="text" placeholder="Search.." name="search"/>
+                <button type="submit">Submit</button>
+                </li>
+                </div>
+                <div className="nav">
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Contact</a></li>
+                    <li><a href="">LogOut</a></li>
+                    <li className="logo"><a href="">Logo</a></li>
+
+                    <div className="details">
+                <p>Img</p>
+               
+            </div>
+                </div>
+            </ul>
+            {/* Profile */}
+           
+        
+        </>
     )
 }
