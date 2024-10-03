728c31020b616e08bdddc982f8782e681d061e8d
Nosihle Mthembu <bongekam16@gmail.com>
2024-10-03 16:45:23 +0200

    Employee header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -1,13 +1,23 @@
+import React from 'react';
+// import './Header.css';
 
-function Header(){
-
-    return(
-        <>
-            <div className="HeaderColor">
-                <div className="user"></div>
+const Header = () => {
+  return (
+    <div class="main-container">
+        <header class="header">
+            <div class="user-profile">
+            <img src="user-profile.jpg" alt="image" class="profile-pic" />
+            <h1>John Doe</h1>
+            <p>Senior Software Engineer</p>
             </div>
-        </>
-    )
-}
+            <div class="search-bar">
+            <input type="text" placeholder="Search..." />
+            <button>Search</button>
+            </div>
+        </header>
+    </div>
 
-export default Header
+  );
+};
+
+export default Header;
